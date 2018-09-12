8236c1731d6fb70fa9c65a7bac8ae1cd109f9c83
DutchMon <dutch.holland13@gmail.com>
2018-09-12 12:43:19 -0700

    container change

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./Container.css";
 
 const Container = () => {
@@ -7,42 +6,48 @@ const Container = () => {
     <div className="container">
       <div className="row text-center">
         <div className="col-lg-4">
-          <Link to="/rmp" title="Rate My Professor" alt="Rate My Professor">
+          <a href="/rmp" title="Rate My Professor" alt="Rate My Professor">
             <img
               src={
-                "https://bloximages.newyork1.vip.townnews.com/laloyolan.com/content/tncms/assets/v3/editorial/2/a2/2a274d71-8277-5bd1-be25-c65cdb710e42/5ac80d20dfc1e.image.jpg?resize=1200%2C600"
+                //"https://bloximages.newyork1.vip.townnews.com/laloyolan.com/content/tncms/assets/v3/editorial/2/a2/2a274d71-8277-5bd1-be25-c65cdb710e42/5ac80d20dfc1e.image.jpg?resize=1200%2C600"
+                "/rmp.gif"
               }
               alt={"Rate my Professor"}
-            />
-          </Link>
+            /><br/>
+            <h7>Rate My Professor</h7>
+          </a>
         </div>
         <div className="col-lg-4">
-          <Link
-            to="/schedule"
-            title="Question Papers, Scheduler and more!"
-            alt="Question Papers, Scheduler and more!"
+          <a
+            href="/schedule"
+            title="Scheduler"
+            alt="Scheduler"
           >
             <img
               src={
-                "https://sundayskills.com/wp-content/uploads/2018/03/cover-image.jpg"
+                // "https://sundayskills.com/wp-content/uploads/2018/03/cover-image.jpg"
+                "/calendar.gif"
               }
-              alt={"Papers"}
-            />
-          </Link>
+              alt={"Scheduler"}
+            /><br/>
+            <h7>Scheduler</h7>
+          </a>
         </div>
         <div className="col-lg-4">
-          <Link
-            to="/internships"
+          <a
+            href="/internships"
             title="Student internships and full-time jobs"
             alt="Student internships and full-time jobs"
           >
             <img
               src={
-                "https://i1.wp.com/www.metacenterchicago.com/wp-content/uploads/2018/02/the-positive-implications-of-internships-961x600.jpg?fit=961%2C600&ssl=1"
+                //"https://i1.wp.com/www.metacenterchicago.com/wp-content/uploads/2018/02/the-positive-implications-of-internships-961x600.jpg?fit=961%2C600&ssl=1"
+                "/giphy.gif"
               }
               alt={"Internships"}
-            />
-          </Link>
+            /><br/>
+            <h7>Job Search</h7>
+          </a>
         </div>
       </div>
     </div>
