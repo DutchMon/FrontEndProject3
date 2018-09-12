1a7a0abc43fb5e42e72739c54752bd6d6d09a8f5
DutchMon <dutch.holland13@gmail.com>
2018-09-12 09:56:47 -0700

    api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,24 +1,45 @@
 // import React from "react";
 import axios from "axios";
 
-// var keys = require("../keys");
+var keys = require("../keys");
 
 export default {
-    //job, type, country, city category, type, location, keywords
-    getJobsAuthenticJobs : () => {
-        // if(!job && !country && !city && !type){
-            return axios.get("https://student-hub-heroku.herokuapp.com/https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key=" + "fd79f0c01d6945b2fde8f095428800e9" + "&method=aj.jobs.search&format=json")
-            // }
-        },
-    getJobCategories: () => {
-        return axios.get("https://student-hub-heroku.herokuapp.com/https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key="+ "fd79f0c01d6945b2fde8f095428800e9" + "&method=aj.categories.getList&format=json")
-    },
+  saveArticle: data => {
+    return axios.post("/savedJobs", data);
+  },
 
-    getAdzunaJobs: () => {
-        return axios.get("https://student-hub-heroku.herokuapp.com/http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=17711746&app_key=cd7075866de3337b020259dba06d0b68&results_per_page=20&what=javascript%20developer&content-type=application/json")
-    },
+  getArticles: data => {
+    return axios.get("/savedJobs", data);
+  },
 
-    saveArticle: (data) => {
-        return axios.post("https://student-hub-heroku.herokuapp.com/saved", data);
-    }
-}
+  //job, type, country, city category, type, location, keywords
+  getJobsAuthenticJobs: () => {
+    // if(!job && !country && !city && !type){
+    return axios.get(
+      "https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key=" +
+        "fd79f0c01d6945b2fde8f095428800e9" +
+        "&method=aj.jobs.search&format=json"
+    );
+    // }
+  },
+  getJobCategories: () => {
+    return axios.get(
+      "https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key=" +
+      "fd79f0c01d6945b2fde8f095428800e9" +
+        "&method=aj.categories.getList&format=json"
+    );
+  },
+
+  getAdzunaJobs: (category, country) => {
+    let url = `https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=17711746&app_key=cd7075866de3337b020259dba06d0b68&results_per_page=20&content-type=application/json&what=${category}&where=${country
+      .split(" ")
+      .join("%20")}`;
+    return axios.get(url);
+  },
+
+  getAdzunaJobCategories: () => {
+    return axios.get(
+      "https://api.adzuna.com/v1/api/jobs/gb/categories?app_id=17711746&app_key=cd7075866de3337b020259dba06d0b68&&content-type=application/json"
+    );
+  }
+};
